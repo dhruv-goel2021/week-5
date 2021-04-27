ca96bdc9dfc2247bdf5a273d7637d2afad8b81b7
Dhruv Goel <dhruv.goel@kellogg.northwestern.edu>
2021-04-27 11:26:54 -0500

    completed lab

--- code-along/events.js
+++ code-along/events.js
@@ -1,19 +1,51 @@
 // Get a reference to the page title element from the DOM
+//let pageTitleElement = document.querySelector(`.page-title`)
+
+// Modify the page title element's contents
+//pageTitleElement.innerHTML = `Movies to watch!`
+
+// Get a reference to the bulleted list of movies
+//let movieList = document.querySelector(`.movies-to-watch`)
+
+// Add an new list item to the bulleted list of movies
+//movieList.insertAdjacentHTML(`beforeend`, `
+//  <li>Spaceballs</li>
+//`)
+
+// Get a reference to the image
+//let image = document.querySelector(`img`)
+
+// Add the `border-pink-500` class to the image element
+//image.classList.add(`border-pink-500`)
+
+// get a reference to the "click me" button
+
+let clickMeButton = document.querySelector(`click-me`)
+
+// event listener for the "click me" button
+
+clickMeButton.addEventListener(`click`, async function (event) {
+
+ //alert(`button was clicked`) //this code essentially tells the button to show you a pop up that says the button was clicked
+
+ // Get a reference to the page title element from the DOM
 let pageTitleElement = document.querySelector(`.page-title`)
 
 // Modify the page title element's contents
 pageTitleElement.innerHTML = `Movies to watch!`
 
-// Get a reference to the bulleted list of movies
+
+})
+
+// get a reference to the "add movie" link
+let addMovieLink = document.querySelector(`.add-movie`)
+
+
+// event listener for the "add movie" link
+addMovieLink.addEventListener(`click`, async function(event){
 let movieList = document.querySelector(`.movies-to-watch`)
 
-// Add an new list item to the bulleted list of movies
 movieList.insertAdjacentHTML(`beforeend`, `
-  <li>Spaceballs</li>
-`)
-
-// Get a reference to the image
-let image = document.querySelector(`img`)
-
-// Add the `border-pink-500` class to the image element
-image.classList.add(`border-pink-500`)
+//  <li>Spaceballs</li>
+//`)
+})
--- lab/week5.js
+++ lab/week5.js
@@ -13,28 +13,46 @@
 // - Homework: Complete the application by accepting a number of days; show the current weather 
 //             conditions and forecast based on the number of days entered by the user.
 
-window.addEventListener('DOMContentLoaded', async function() {
+window.addEventListener('DOMContentLoaded', async function(event) {
   // Get a reference to the "get weather" button
-
+let getWeatherButton = document.querySelector(`.get-weather`)
   // When the "get weather" button is clicked:
+getWeatherButton.addEventListener(`click`, async function(event) {
 
     // - Ignore the default behavior of the button
-
+event.preventDefault()
     // - Get a reference to the element containing the user-entered location
-
+let locationInput = document.querySelector(`#location`)
     // - Get the user-entered location from the element's value
-
+let location = locationInput.value
     // - Check to see if the user entered anything; if so:
-
+if (location.length > 0 ) {
       // - Construct a URL to call the WeatherAPI.com API
-
+let url = `https://api.weatherapi.com/v1/forecast.json?key=789177a04d814163804160922212704&q=${location}$days=3`
       // - Fetch the url, wait for a response, store the response in memory
-
+let response = await fetch(url)
       // - Ask for the json-formatted data from the response, wait for the data, store it in memory
-
+let json = await response.json()
       // - Write the json-formatted data to the JavaScript console
-
+console.log(json)
       // - Store the interpreted location, current weather conditions, the forecast as three separate variables
-
+let current = json.current  //object represents current weather
+let locationCurrent = json.location //object represents current location
+let forecast = json.forecast //object represents forecast
       // - Continue the recipe yourself!
+
+      // write inputted city into header title 
+let cityInput = document.querySelector(`.current`)
+cityInput.innerHTML = `
+<div class="text-center space-y-2">
+<div class="font-bold text-3xl">Current Weather for ${locationCurrent.name}, ${locationCurrent.region}</div>
+<div class="font-bold">
+  <img src="https://cdn.weatherapi.com/weather/64x64/day/116.png" class="inline-block">
+  <span class="temperature">60</span>° 
+  and
+  <span class="conditions">Partly Cloudy</span>
+</div>
+</div>`
+}
 })
+    })
